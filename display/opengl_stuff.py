3abc5cef84f7a5332c95a6be524083cbfe31af6b
TheBookwyrms <blackdragon6493@gmail.com>
2025-03-17 23:37:44 -0400

    clean up display code, add some documentation

--- display/opengl_stuff.py
+++ display/opengl_stuff.py
@@ -11,20 +11,49 @@ class OpenGLStuff:
         # class initialisation, nothing goes here
         pass
 
+    def make_quads(self, base_x, x_increment, base_y, y_increment, base_z, z_change, colour):
+
+        '''
+        one base has changing basepoints and x_increment of rod width
+        one base has fixed basepoint and square side length increment
+        z starts from box base z and add box z increment
+        '''
+
+        p1 = np.array([base_x,             base_y,             base_z,          colour[0], colour[1], colour[2], 1]) # base_point + (0,    0,    0)    # BFL
+        p2 = np.array([base_x+x_increment, base_y,             base_z,          colour[0], colour[1], colour[2], 1]) # base_point + (xlen, 0,    0)    # BFR
+        p3 = np.array([base_x,             base_y+y_increment, base_z,          colour[0], colour[1], colour[2], 1]) # base_point + (0,    ylen, 0)    # BBL
+        p4 = np.array([base_x+x_increment, base_y+y_increment, base_z,          colour[0], colour[1], colour[2], 1]) # base_point + (xlen, ylen, 0)    # BBR
+        p5 = np.array([base_x,             base_y,             base_z+z_change, colour[0], colour[1], colour[2], 1]) # base_point + (0,    0,    zlen) # TFL
+        p6 = np.array([base_x+x_increment, base_y,             base_z+z_change, colour[0], colour[1], colour[2], 1]) # base_point + (xlen, 0,    zlen) # TFR
+        p7 = np.array([base_x,             base_y+y_increment, base_z+z_change, colour[0], colour[1], colour[2], 1]) # base_point + (0,    ylen, zlen) # TBL
+        p8 = np.array([base_x+x_increment, base_y+y_increment, base_z+z_change, colour[0], colour[1], colour[2], 1]) # base_point + (xlen, ylen, zlen) # TBR
+
+        qf = np.array([p1, p2, p6, p5]).astype(np.float32) # quad front
+        qb = np.array([p3, p4, p8, p7]).astype(np.float32) # quad back
+        ql = np.array([p1, p3, p7, p5]).astype(np.float32) # quad left
+        qr = np.array([p2, p4, p8, p6]).astype(np.float32) # quad right
+        qB = np.array([p1, p3, p4, p2]).astype(np.float32) # quad bottom
+        qT = np.array([p5, p7, p8, p6]).astype(np.float32) # quad top
+
+        return (qf, qb, ql, qr, qB, qT)
+
+
     def setup(self):
         # setup of all elements to be rendered on each loop
 
 
-        z_depth_per_two = 3
-        num_doubles = 8
+        num_doubles = 5
         initial_x, initial_y, initial_z = 0, 0, 0
         square_side_len = 8
+
+        width_per_one = 3
+        dead_space = 2
 
         all_data = []
 
         for double in range(num_doubles):
-            z_change = z_depth_per_two/2
-            base_x, base_y, base_z = initial_x, initial_y, initial_z + double*z_depth_per_two
+            z_change = width_per_one
+            base_x, base_y, base_z = initial_x, initial_y, initial_z + double*(2*width_per_one+2*dead_space)
 
             num_rods = 2**(double+1)
 
@@ -36,58 +65,44 @@ class OpenGLStuff:
 
                 z_start = base_z
 
-
                 colour = [1, 0, 0] if rod%2==0 else [0, 1, 0]
 
-                p1 = np.array([x,                 base_y,                 z_start,          colour[0], colour[1], colour[2], 1]) # # BFL
-                p2 = np.array([x+dist_between_x_basepoints, base_y,                 z_start,          colour[0], colour[1], colour[2], 1]) # base_point+(xlen, 0, 0) # BFR
-                p3 = np.array([x,                 base_y+square_side_len, z_start,          colour[0], colour[1], colour[2], 1]) # base_point+(0, ylen, 0) # BBL
-                p4 = np.array([x+dist_between_x_basepoints, base_y+square_side_len, z_start,          colour[0], colour[1], colour[2], 1]) # base_point+(xlen, ylen, 0) # BBR
-                p5 = np.array([x,                 base_y,                 z_start+z_change, colour[0], colour[1], colour[2], 1]) # base_point+(0, 0, zlen) # TFL
-                p6 = np.array([x+dist_between_x_basepoints, base_y,                 z_start+z_change, colour[0], colour[1], colour[2], 1]) # base_point+(xlen, 0, zlen) # TFR
-                p7 = np.array([x,                 base_y+square_side_len, z_start+z_change, colour[0], colour[1], colour[2], 1]) # base_point+(0, ylen, zlen) # TBL
-                p8 = np.array([x+dist_between_x_basepoints, base_y+square_side_len, z_start+z_change, colour[0], colour[1], colour[2], 1]) # base_point+(xlen, ylen, zlen) # TBR
-
-                qf = np.array([p1, p2, p6, p5]).astype(np.float32)
-                qb = np.array([p3, p4, p8, p7]).astype(np.float32)
-                ql = np.array([p1, p3, p7, p5]).astype(np.float32)
-                qr = np.array([p2, p4, p8, p6]).astype(np.float32)
-                qB = np.array([p1, p3, p4, p2]).astype(np.float32)
-                qT = np.array([p5, p7, p8, p6]).astype(np.float32)
-
-                all_data.extend((qf, qb, ql, qr, qB, qT))
+                all_data.extend(
+                    self.make_quads(
+                        base_x      = x,                         # individual x per basepoint
+                        x_increment = dist_between_x_basepoints, # rod width
+                        base_y      = base_y,                    # box base y
+                        y_increment = square_side_len,           # length of square
+                        base_z      = z_start,                   # initial z of box
+                        z_change    = z_change,                  # z depth of box
+                        colour      = colour,                    # colour
+                    )
+                )
 
 
             "yx"
             y_basepoints = np.linspace(base_y, base_y+square_side_len, num_rods, endpoint=False)
             dist_between_y_basepoints = x_basepoints[1]-x_basepoints[0]
 
-
             for rod, y in enumerate(y_basepoints):
 
-                z_start = base_z+z_depth_per_two/2
+                z_start = base_z+width_per_one+dead_space
                 
                 colour = [1, 0, 0] if rod%2==0 else [0, 1, 0]
 
-                p1 = np.array([base_x,                 y,                 z_start,          colour[0], colour[1], colour[2], 1]) # # BFL
-                p2 = np.array([base_x+square_side_len, y,                 z_start,          colour[0], colour[1], colour[2], 1]) # base_point+(xlen, 0, 0) # BFR
-                p3 = np.array([base_x,                 y+dist_between_y_basepoints, z_start,          colour[0], colour[1], colour[2], 1]) # base_point+(0, ylen, 0) # BBL
-                p4 = np.array([base_x+square_side_len, y+dist_between_y_basepoints, z_start,          colour[0], colour[1], colour[2], 1]) # base_point+(xlen, ylen, 0) # BBR
-                p5 = np.array([base_x,                 y,                 z_start+z_change, colour[0], colour[1], colour[2], 1]) # base_point+(0, 0, zlen) # TFL
-                p6 = np.array([base_x+square_side_len, y,                 z_start+z_change, colour[0], colour[1], colour[2], 1]) # base_point+(xlen, 0, zlen) # TFR
-                p7 = np.array([base_x,                 y+dist_between_y_basepoints, z_start+z_change, colour[0], colour[1], colour[2], 1]) # base_point+(0, ylen, zlen) # TBL
-                p8 = np.array([base_x+square_side_len, y+dist_between_y_basepoints, z_start+z_change, colour[0], colour[1], colour[2], 1]) # base_point+(xlen, ylen, zlen) # TBR
+                all_data.extend(
+                    self.make_quads(
+                        base_x      = base_x,                    # box base x
+                        x_increment = square_side_len,           # length of square
+                        base_y      = y,                         # individual x per basepoint
+                        y_increment = dist_between_y_basepoints, # rod width
+                        base_z      = z_start,                   # initial z of box
+                        z_change    = z_change,                  # z depth of box
+                        colour      = colour,                    # colour
+                    )
+                )
 
-                qf = np.array([p1, p2, p6, p5]).astype(np.float32)
-                qb = np.array([p3, p4, p8, p7]).astype(np.float32)
-                ql = np.array([p1, p3, p7, p5]).astype(np.float32)
-                qr = np.array([p2, p4, p8, p6]).astype(np.float32)
-                qB = np.array([p1, p3, p4, p2]).astype(np.float32)
-                qT = np.array([p5, p7, p8, p6]).astype(np.float32)
 
-                all_data.extend((qf, qb, ql, qr, qB, qT))
-
-        
         all_vbo = []
         for quad in all_data:
             all_vbo.append(make_vbo(quad))
