4aab149cf21df766bbec920d0d948579c568e947
Anodymous1 <andy.co.yu@gmail.com>
2025-03-30 15:04:09 -0400

    Added binary-to-list function

--- display/display_2/Visualizer/test(me).py
+++ display/display_2/Visualizer/test(me).py
@@ -1,6 +1,30 @@
 import random
 
+def bin_to_list(bin):
+    bit = 24
+    list = []
+    for i in range(bit//2):
+
+        first_two  = (bin >> ((bit-2) - 2 * i))&3
+
+        if first_two == 2:
+
+            list.append((1,0))
+
+        elif first_two == 1:
+
+            list.append((0,1))
+        
+        else:
+            return False    #error
+    
+    return list
+
+print(bin_to_list(int(0b101010101010101010101001)))
+
+
 def bit8(var):
+
     bit8 = 0
     for i in range(0, 8):
 
