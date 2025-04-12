23bf92d8c8820508df44de8f179e004fb34011b7
ThatAquarel <xia_tianyi@outlook.com>
2025-04-12 19:27:01 -0400

    add logo + text

--- software/signal_display/scintillator_display/display/impl_controls/controls.py
+++ software/signal_display/scintillator_display/display/impl_controls/controls.py
@@ -3,6 +3,8 @@ import scintillator_display.compat.imgui as imgui
 
 from OpenGL.GL import glClearColor, glClear, GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT
 
+from .load_res import load_texture, load_font
+from scintillator_display.display.impl_a.graphics.parameter_interface import ui_section, ui_spacing
 
 class Controls:
     def __init__(self):
@@ -10,6 +12,13 @@ class Controls:
         glfw.set_window_size_callback(self.window, self.window_size_callback)
 
         self.width, self.height = 0, 0
+
+        # ffmpeg -i sc_bw.png -vf "scale=-1:48" hep_48.png
+        self.hep = load_texture("hep_48.png")
+        # ffmpeg -i sc_bw.png -vf "scale=-1:64" sc_64.png
+        self.sc = load_texture("sc_64.png")
+
+        self.font = load_font("Poppins-Regular.ttf", 24)
 
     def window_size_callback(self, window, width, height):
         self.width, self.height = width, height
@@ -21,12 +30,24 @@ class Controls:
         imgui.set_next_window_position(0, 0, condition=imgui.ONCE)
         imgui.set_next_window_size(self.width, self.height, condition=imgui.ALWAYS)
         imgui.begin(
-            "System Display",
+            " ",
             flags=imgui.WINDOW_NO_RESIZE
             | imgui.WINDOW_NO_MOVE
             | imgui.WINDOW_NO_COLLAPSE,
         )
 
-        imgui.text("dslkfajfdslkajdfklsajklsdfajkldsfjklfd")
+        imgui.separator()
+
+        imgui.push_font(self.font)
+        imgui.text("The Scintillating Chamber")
+        imgui.pop_font()
+
+        imgui.separator()
+
+        imgui.image(*self.hep)
+        imgui.same_line()
+        imgui.image(*self.sc)
+
+        imgui.separator()
 
         imgui.end()
